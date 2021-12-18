60a5c142ac8727df448bbbae78bf99c91b41deb5
Nasyrov Sultan <tightbtctrader@gmail.com>
2021-12-18 12:18:36 +0300

    Update dockerfile and add docker-compose.yaml

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,15 @@
-FROM python:3.8-bullseye
+FROM python:3.8
 
 # set python specific environment variables
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
+ENV DJANGO_SETTINGS_MODULE=mind_palace.settings
 
 # install python dependencies
 RUN apt-get update && apt-get upgrade
 
 # install application requirements
-RUN mkdir /app
-COPY . /app
 WORKDIR /app
+COPY requirements.txt /app/
 RUN pip install -r requirements.txt
-
-ENTRYPOINT ["python", "manage.py runserver"]
+COPY . /app
--- mind_palace/settings.py
+++ mind_palace/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -120,14 +121,14 @@ WSGI_APPLICATION = 'mind_palace.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mind_palace',
-        'HOST': 'localhost',
-        'PORT': '',
-        'USER': 'developer',
-        'PASSWORD': '123456'
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'postgres',
+        'PORT': 5432,
     }
-}
 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
