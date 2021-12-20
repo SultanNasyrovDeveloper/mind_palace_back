c2c88f0afff1e896451141600dbcaa51e4710ce8
Nasyrov Sultan <tightbtctrader@gmail.com>
2021-12-20 10:28:58 +0300

    Update node views.

--- mind_palace/palace/node/views.py
+++ mind_palace/palace/node/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
 from mind_palace.palace.node import models, serializers
@@ -26,6 +27,19 @@ class MindPalaceNodeViewSet(viewsets.ModelViewSet):
         stats.views += 1
         stats.save()
         return Response(self.serializer_class(node).data)
+
+    @action(detail=False, methods=('GET',))
+    def tree(self, request, *args, **kwargs):
+        root_id = request.GET.get('root', None)
+        if not root_id:
+            raise ValidationError('Root must be specified.')
+        depth = request.GET.get('depth', 3)
+        root_node = models.MindPalaceNode.objects.get(id=root_id)
+        cached_tree_root = root_node.get_descendants(include_self=True).filter(
+            level__lt=root_node.level + int(depth)
+        ).get_cached_trees()[0]
+        serializer = serializers.MindPalaceTreeNodeSerializer(cached_tree_root)
+        return Response(serializer.data)
 
     @action(
         detail=True,
